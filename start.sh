d86c0b3784f42835e5dc31f8b4b5fe2a90f3cab4
BulbaWarrior <vladkalm49@gmail.com>
2021-07-13 13:17:59 +0300

    Pass several variables instead of DATABASE_URL

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-
+if [[ $POSTGRES_USER && $POSTGRES_PASSWORD && $POSTGRES_HOST ]]; then
+   export DATABASE_URL="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST"
+fi
+echo "using database url: $DATABASE_URL"
 ./node_modules/.bin/node-pg-migrate up
 nodemon server
 exec "$@"
